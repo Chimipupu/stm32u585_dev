2fe640afae3231f944de80aee68a7571ac30ea48
Chimipupu <chiminyanyan@gmail.com>
2026-06-08 21:39:55 +0900

    LPUARTで8ByteのFIFOを有効（FIFOなしだと1Byteずつ割り込み来て鬱陶しい）

--- src/stm32u585ci_dev/App/app_main.c
+++ src/stm32u585ci_dev/App/app_main.c
@@ -41,23 +41,20 @@ static void _DbgCmdTask(void *p_args);
 // NOTE: STM32U585にはバックアップSRAMの「BKPSRAM」が 2KB ある
 
 // Bootカウント
-volatile uint32_t g_bk_sram_boot_cnt __attribute__((section(".bk_sram"))) = 0;
+uint32_t g_bkram_boot_cnt __attribute__((section(".bk_sram"))) = 0;
 
 // --------------------------------------------------------------------------
-volatile static bool s_rx_uart_cmd_flg = false;
-
-#define LPUART_RX_BUF_SIZE    256
+#define LPUART_RX_BUF_SIZE    128
 static volatile uint8_t s_lpuart_rx_buf[LPUART_RX_BUF_SIZE];
+#define UART_CMD_RX_BUF_SIZE    64
+static volatile uint8_t s_uart_cmd_rx_buf[UART_CMD_RX_BUF_SIZE];
 static uint8_t s_rx_buf_idx = 0;
-
-
-#ifdef DBG_APP
-static void _rtc_update(void);
-#endif // DBG_APP
+static bool s_rx_uart_cmd_flg = false;
+// static void _rtc_update(void);
 // --------------------------------------------------------------------------
 // [Static]
 
-#ifdef DBG_APP
+#if 0
 static void _rtc_update(void)
 {
     RTC_DateTypeDef sdatestructureget;
@@ -73,43 +70,45 @@ static void _rtc_update(void)
                 stimestructureget.Hours,stimestructureget.Minutes,stimestructureget.Seconds);
     }
 }
-#endif // DBG_APP
+#endif
 
 static void _AppMainTask(void *p_args)
 {
     // バックアップSRAMのBootカウントをインクリメント
-    DBG_LPUART_PRINTF("[DEBUG] Boot Cnt = %d\r\n", g_bk_sram_boot_cnt);
-    g_bk_sram_boot_cnt++;
+    DBG_LPUART_PRINTF("[DEBUG] Boot Cnt = %d\r\n", g_bkram_boot_cnt);
+    g_bkram_boot_cnt++;
 
     DBG_LPUART_PRINTF("[AppMainTask]: Init\r\n");
 
     while (1)
     {
-#ifdef DBG_APP
         HAL_GPIO_TogglePin(PCB_LED_PORT, PCB_LED_PIN);
-        _rtc_update(); // RTC更新
-#endif // DBG_APP
-
-        osDelay(100);
+        // _rtc_update(); // RTC更新
+        osDelay(20);
     }
 }
 
 static void _DbgCmdTask(void *p_args)
 {
     bool ret;
-    uint8_t cmd_buf[64] = {0};
 
     DBG_LPUART_PRINTF("[DbgCmdTask]: Init\r\n");
 
     while (1)
     {
-        ret = dbg_cmd_ready(cmd_buf);
-        if (ret != false) {
-            DBG_LPUART_PRINTF("[DEBUG] Cmd: %s\r\n", cmd_buf);
-            memset(&cmd_buf[0], 0x00, sizeof(cmd_buf));
+        if(s_rx_uart_cmd_flg != false) {
+            ret = dbg_cmd_ready((uint8_t *) &s_uart_cmd_rx_buf[0]);
+            if (ret != false) {
+                DBG_LPUART_PRINTF("[DEBUG] Cmd: %s\r\n", s_uart_cmd_rx_buf);
+
+                // TODO: コマンドのテーブル検索とコールバック関数の実行の実装
+
+                // バッファ初期化
+                memset((void *) &s_uart_cmd_rx_buf[0], 0x00, sizeof(s_uart_cmd_rx_buf));
+            }
         }
 
-        osDelay(300);
+        osDelay(100);
     }
 }
 
@@ -138,58 +137,59 @@ void DBG_LPUART_PRINTF(const char *format, ...)
 
 bool dbg_cmd_ready(uint8_t *p_cmd_buf)
 {
-    bool ret = false;
     uint8_t i;
     uint8_t *p_ptr = p_cmd_buf;
 
-    // NOTE: コマンドを渡す(将来的にはDMAにさせたい)
-    if(s_rx_uart_cmd_flg != false) {
-        for(i = 0; i < s_rx_buf_idx; i++)
-        {
-            *p_ptr = s_lpuart_rx_buf[i];
-            p_ptr++;
+    for(i = 0; i < s_rx_buf_idx; i++)
+    {
+        if((s_lpuart_rx_buf[i] == '\r') || (s_lpuart_rx_buf[i] == '\n')) {
+            break;
         }
 
-        ret = true;
-
-        // 変数初期化
-        memset((void *)&s_lpuart_rx_buf[0], 0x00, LPUART_RX_BUF_SIZE);
-        s_rx_buf_idx = 0;
-        s_rx_uart_cmd_flg = false;
+        *p_ptr = s_lpuart_rx_buf[i];
+        p_ptr++;
     }
 
-    return ret;
+    // 変数初期化
+    memset((void *)&s_lpuart_rx_buf[0], 0x00, LPUART_RX_BUF_SIZE);
+    s_rx_buf_idx = 0;
+    s_rx_uart_cmd_flg = false;
+
+    return true;
 }
 
 void lpuart1_irq_handler(void)
 {
-    volatile uint8_t tmp;
+    uint8_t tmp;
 
-    /* RXNE */
-    if (LL_LPUART_IsActiveFlag_RXNE(LPUART1)) {
+#if 0
+    // ORE （オーバーランエラー）
+    if (LL_LPUART_IsActiveFlag_ORE(LPUART1)) {
+        LL_LPUART_ClearFlag_ORE(LPUART1);
+    }
+#endif
+
+    /**
+    * @brief RXFNE (Receive FIFO Not Empty) をチェック
+    * @note LPUARTのFIFOサイズ = 8Byte
+    * @note RXFNEの割り込みはFIFOが1/8埋まったら来る
+    * @note CPUが割り込みハンドラに来るまでに残りの7Byteは埋まるからORE（オーバーランエラー）は起きにくい、はず
+    */
+    while (LL_LPUART_IsActiveFlag_RXNE_RXFNE(LPUART1))
+    {
         tmp = (uint8_t)LL_LPUART_ReceiveData8(LPUART1);
-
-        // ASCIIの文字か特定の文字だけバッファで受け取る
-        if ((tmp >= '0' && tmp <= '9') ||
-            (tmp >= 'A' && tmp <= 'Z') || (tmp >= 'a' && tmp <= 'z') ||
-            (tmp == '!') || (tmp == '?'))
-        {
-            s_lpuart_rx_buf[s_rx_buf_idx] = tmp;
-            s_rx_buf_idx = (s_rx_buf_idx + 1) % LPUART_RX_BUF_SIZE;
-        // コマンド受信 = デリミタ
-        } else if ((s_rx_buf_idx > 0) && (tmp == '\r' || tmp == '\n')) {
+        if ((tmp == '\r') || (tmp == '\n')) {
             s_rx_uart_cmd_flg = true;
         }
+
+        s_lpuart_rx_buf[s_rx_buf_idx] = tmp;
+        s_rx_buf_idx = (s_rx_buf_idx + 1) % LPUART_RX_BUF_SIZE;
     }
 }
 
 void app_main_init(void)
 {
-    DBG_LPUART_PRINTF("STM32U585CIU6 Develop by Chimipupu\r\n");
-
-#ifdef DBG_APP
-    _mcu_test();
-#endif // DBG_APP
+    DBG_LPUART_PRINTF("STM32H562VGT6 Develop by Chimipupu\r\n");
 
     // アプリのメイン用のFreeRTOSタスクを生成
     AppMainTaskHandle = osThreadNew(_AppMainTask, NULL, &AppMainTask_Attr);
